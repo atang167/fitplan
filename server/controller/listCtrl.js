ac047548275a4e26b7797a6e58c89f3954990bcb
Angie Tang <atang167@gmail.com>
2018-02-25 13:43:01 -0800

    Render json list

--- server/controller/listCtrl.js
+++ server/controller/listCtrl.js
@@ -7,12 +7,13 @@ const listUrl = process.env.LIST_URL;
 module.exports = {
   getList: (req, res) => {
     axios.get(listUrl)
-    .then((response) => {
-      res.send({'list': response.data})
+    .then((data) => {
+      res.send(data);
     })
     .catch(err => {
       res.status(500).send(err);
     })
   }
 }
+
 
