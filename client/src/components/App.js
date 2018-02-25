91ca8555a0e0c80dada4060ec063c43cc885c2af
Angie Tang <atang167@gmail.com>
2018-02-25 15:15:01 -0800

    Get list using request to db

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import List from './List'
 
 class App extends Component {
   constructor(props) {
@@ -7,7 +8,7 @@ class App extends Component {
   
   render() {
     return (
-      <div>This is home page</div>
+      <List /> 
     )
   }
 }
---
+++ client/src/components/List.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'React'
+import axios from 'axios'
+
+class List extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      list: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/list')
+    .then(response => {
+      this.setState({list: response}); 
+      console.log(response); 
+    })
+    .catch(err => {
+      console.log('err getting list', err);
+    })
+  }
+  
+  render() {
+    return (
+      <div></div> 
+    )
+  }
+}
+
+export default List; 
