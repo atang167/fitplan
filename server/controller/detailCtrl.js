708f52984c016a4ceeb6cd45b4a8ca7c3e1fc99d
Angie Tang <atang167@gmail.com>
2018-02-25 20:20:42 -0800

    Add detail controller

---
+++ server/controller/detailCtrl.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+require('dotenv').config();
+
+const detailUrl = process.env.DETAIL_URL;
+
+module.exports = {
+  getList: (req, res) => {
+    axios.get(detailUrl)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
+  }
+}
