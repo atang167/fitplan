1c1fd97db39051670f94ac06a609cb8ec703ed78
Angie Tang <atang167@gmail.com>
2018-02-25 20:19:44 -0800

    Add categories to list

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const ListItem = props => (
-  <div>
-    <div>{props.name}</div>
-    <div>{props.athleteName}</div>
-    <img src={props.url}></img>
+const ListItem = (props) => {
+  console.log(props, 'props');
+  return (
+    <div>
+      <div>{props.name}</div>
+      <div>{props.athleteName}</div>
+      <img src={props.url}></img>
+      <div>{props.femaleBooty? 'Female Booty' : 'Null'}</div>
+      <div>{props.femaleBuild? 'Female Build' : 'Null'}</div>
+      <div>{props.femaleShred? 'Female Shred' : 'Null'}</div>
+      <div>{props.femaleTone? 'Female Tone' : 'Null'}</div>
+      <div>{props.maleAthletic? 'Male Athletic' : 'Null'}</div>
+      <div>{props.maleBulk? 'Male Bulk' : 'Null'}</div>
+      <div>{props.maleShred? 'Male Shred' : 'Null'}</div>
   </div>
-);
+  )
+}
 
 export default ListItem;
